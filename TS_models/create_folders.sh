32be51236fe05cbf024f45e7918e4cd48da7d0cc
CEitanC <50078327+CEitanC@users.noreply.github.com>
2023-06-21 16:25:53 +0300

    Create create_folders.sh

---
+++ TS_models/create_folders.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create folders
+mkdir ten_fold_250
+mkdir ten_fold
+mkdir EX1
+mkdir EX2
+mkdir EX3
+mkdir EX4
+mkdir learners_EX1
+mkdir learners_EX2
+mkdir learners_EX3
+mkdir learners_EX4
+mkdir light_output
+mkdir lightning_saved_params
+
+# Print success message
+echo "Folders created successfully!"
